9d8650f303382e99bbfa69eb17ae5db6f11380e5
MayanthaS <sapumalmayantha@gmail.com>
2026-01-26 00:36:57 +0530

    Refactor Fruits component to use an array of objects for fruit data with name and price

--- src/components/Fruits.jsx
+++ src/components/Fruits.jsx
@@ -1,11 +1,31 @@
 export default function Fruits() {
-  const fruits = ["Apple", "Mango", "Banana", "Orange"];
+  // const fruits = ["Apple", "Mango", "Banana", "Orange"];
+  const fruits = [
+    {
+      name: "Apple",
+      price: "10",
+    },
+    {
+      name: "Banana",
+      price: "20",
+    },
+    {
+      name: "Orange",
+      price: "30",
+    },
+    {
+      name: "PineApple",
+      price: "25",
+    },
+  ];
 
   return (
     <div>
-      {fruits.map((fruit) => (
-        <ul>
-          <li key={fruit}>{fruit}</li>
+      {fruits.map((fruits) => (
+        <ul key={fruits}>
+          <li>
+            {fruits.name} :${fruits.price}
+          </li>
         </ul>
       ))}
     </div>
